ec892d99fe54cb481eba77f19fe9d4f5ea9031c6
Nick Morgan <nickm@twitter.com>
2015-09-23 23:40:58 -0500

    Hook up forth to editor.js

--- editor.js
+++ editor.js
@@ -5,15 +5,18 @@ function Editor(selector) {
   var $prevLines = $editor.find(".prev-lines");
   var $input = $editor.find("input");
 
-  function addLine(text) {
-    var $newLine = $("<p>").text(text);
+  function addLine(code, output) {
+    var $codeSpan = $("<span>").addClass("code").text(code);
+    var $outputSpan = $("<span>").addClass("output").text(output);
+    var $newLine = $("<p>").append($codeSpan, $outputSpan);
     $newLine.appendTo($prevLines);
   }
 
   $input.on("keydown", function (e) {
     if (e.keyCode === 13) { // Enter/Return
-      addLine($input.val());
+      var code = $input.val();
       $input.val("");
+      addLine(code, forth.readLine(code));
     }
   });
 }
