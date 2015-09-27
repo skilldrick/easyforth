b86b5d6e48f217e3f3f97f2cdbeff99c42677085
Nick Morgan <nickm@twitter.com>
2015-09-27 00:18:07 -0500

    Add a bunch of definitions

--- forth.js
+++ forth.js
@@ -88,6 +88,9 @@ function Forth() {
     return "";
   }
 
+  ["if", "else", "then", "do", "loop"].forEach(function (code) {
+    dictionary.add(code, controlCode(code));
+  });
 
   dictionary.add(".",  function (stack, dictionary, returnStack) {
     return stack.pop() + " ";
@@ -99,6 +102,11 @@ function Forth() {
 
   dictionary.add("+", function (stack, dictionary, returnStack) {
     stack.push(stack.pop() + stack.pop());
+  });
+
+  dictionary.add("-", function (stack, dictionary, returnStack) {
+    var a = stack.pop(), b = stack.pop();
+    stack.push(b - a);
   });
 
   dictionary.add("*", function (stack, dictionary, returnStack) {
@@ -135,12 +143,36 @@ function Forth() {
     stack.push(b > a ? TRUE : FALSE);
   });
 
+  dictionary.add("and", function (stack, dictionary, returnStack) {
+    var a = stack.pop(), b = stack.pop();
+    stack.push(a !== FALSE && b !== FALSE ? TRUE : FALSE);
+  });
+
+  dictionary.add("or", function (stack, dictionary, returnStack) {
+    var a = stack.pop(), b = stack.pop();
+    stack.push(a !== FALSE || b !== FALSE ? TRUE : FALSE);
+  });
+
   dictionary.add("i", function (stack, dictionary, returnStack) {
     stack.push(returnStack.peek(1));
   });
 
   dictionary.add("j", function (stack, dictionary, returnStack) {
     stack.push(returnStack.peek(2));
+  });
+
+  // I don't understand the difference between i and r@
+  // http://www.forth.com/starting-forth/sf5/sf5.html
+  dictionary.add("r@", function (stack, dictionary, returnStack) {
+    stack.push(returnStack.peek(1));
+  });
+
+  dictionary.add(">r", function (stack, dictionary, returnStack) {
+    returnStack.push(stack.pop());
+  });
+
+  dictionary.add("r>", function (stack, dictionary, returnStack) {
+    stack.push(returnStack.pop());
   });
 
   dictionary.add("emit", function (stack, dictionary, returnStack) {
@@ -177,17 +209,24 @@ function Forth() {
     stack.pop();
   });
 
-  ["if", "else", "then", "do", "loop"].forEach(function (code) {
-    dictionary.add(code, controlCode(code));
-  });
-
   readLine(": cr  10 emit ;");
-
   readLine(": space  32 emit ;");
   readLine(": spaces  0 do space loop ;");
   readLine(": 0=  0 = ;");
   readLine(": 0<  0 < ;");
   readLine(": 0>  0 > ;");
+  readLine(": ?dup  dup if dup then ;");
+  readLine(": 2dup  over over ;");
+  readLine(": 1+  1 + ;");
+  readLine(": 1-  1 - ;");
+  readLine(": 2+  2 + ;");
+  readLine(": 2-  2 - ;");
+  readLine(": 2*  2 * ;");
+  readLine(": 2/  2 / ;");
+  readLine(": negate  -1 * ;");
+  readLine(": abs  dup 0< if negate then ;");
+  readLine(": min  2dup < if drop else swap drop then ;");
+  readLine(": max  2dup < if swap drop else drop then ;");
 
   return {
     readLine: readLine,
