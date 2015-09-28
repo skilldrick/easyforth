3df51eb9d02ebd914a670279057c7acefea2513c
Nick Morgan <nickm@twitter.com>
2015-09-27 19:27:02 -0500

    Reorganize forth.js

--- forth.js
+++ forth.js
@@ -1,34 +1,23 @@
 'use strict';
 
 function Forth() {
-  var inDefinition = false;
-  var currentDefinition = null;
+  // Core structures
   var stack = Stack('Argument Stack');
   var returnStack = Stack('Return Stack');
   var dictionary = Dictionary();
 
-  function isNumber(val) {
-    return +val + "" === val;
-  }
-
-  function invalidWord(word) {
-    if (word !== ";") { // Can safely skip ;
-      throw new MissingWordError(word);
-    }
-  }
-
-  // Throw if error is not of one of these types
-  function throwIfNot(e, errorTypes) {
-    var shouldHandle = errorTypes.some(function (errorType) {
-      return e instanceof errorType;
-    });
-
-    if (!shouldHandle) {
-      throw e;
-    }
-  }
-
+  // Convert token into an action that executes that token's behavior
   function tokenToAction(token) {
+    function isNumber(val) {
+      return +val + "" === val;
+    }
+
+    function invalidWord(word) {
+      if (word !== ";") { // Can safely skip ;
+        throw new MissingWordError(word);
+      }
+    }
+
     var word = token.value;
     var definition = dictionary.lookup(word);
 
@@ -59,10 +48,27 @@ function Forth() {
     }
   }
 
-  function addToDictionary(name, actions) {
+  // compile actions into definition and add definition to dictionary
+  function compileAndAddToDictionary(name, actions) {
     var definition = compile(dictionary, actions);
     dictionary.add(name, definition);
   }
+
+  // Throw if error is not of one of these types
+  function throwIfNot(e, errorTypes) {
+    var shouldHandle = errorTypes.some(function (errorType) {
+      return e instanceof errorType;
+    });
+
+    if (!shouldHandle) {
+      throw e;
+    }
+  }
+
+  // These state variables are shared across multiple calls to readLine,
+  // as definitions can span multiple lines
+  var inDefinition = false;
+  var currentDefinition = null;
 
   function readLine(line) {
     var tokenizer = Tokenizer(line);
@@ -87,7 +93,7 @@ function Forth() {
       }
 
       if (tokenizer.isDefinitionEnd()) {
-        addToDictionary(currentDefinition.name, currentDefinition.actions);
+        compileAndAddToDictionary(currentDefinition.name, currentDefinition.actions);
         inDefinition = false;
         currentDefinition = null;
         return "  ok";
