beed7265990669b2fc52a911a13b60aa9525a338
Nick Morgan <skilldrick@gmail.com>
2015-10-17 20:38:47 -0700

    Clean up some of the unnecessary helper functions in forth.js

--- javascripts/forth.js
+++ javascripts/forth.js
@@ -44,51 +44,10 @@ function Forth() {
     };
   }
 
-  // Convert value to string, but undefined to ""
-  function getString(output) {
-    if (output === undefined) {
-      return "";
-    } else {
-      return "" + output;
-    }
-  }
-
-  function makeActionReturnString(action) {
-    if (action.isControlCode) {
-      // control codes are not functions
-      return action;
-    } else {
-      // If an action returns undefined, make it return a string instead
-      return function (context) {
-        return getString(action(context));
-      };
-    }
-  }
-
-  // iterate through tokens, converting to action function
-  function eachTokenAsAction(tokenizer, callback) {
-    var nextToken;
-    while (nextToken = tokenizer.nextToken()) {
-      var action = tokenToAction(nextToken)
-      callback(makeActionReturnString(action));
-    }
-  }
-
   // compile actions into definition and add definition to dictionary
   function compileAndAddToDictionary(name, actions) {
     var definition = compile(context.dictionary, actions);
     context.dictionary.add(name, definition);
-  }
-
-  // Throw if error is not of one of these types
-  function throwIfNotOneOf(e, errorTypes) {
-    var shouldHandle = errorTypes.some(function (errorType) {
-      return e instanceof errorType;
-    });
-
-    if (!shouldHandle) {
-      throw e;
-    }
   }
 
   function createVariable(name) {
@@ -142,29 +101,26 @@ function Forth() {
   function readLine(line) {
     var tokenizer = Tokenizer(line);
 
-    var output = "";
+    var output = [];
+    var nextToken;
 
     try {
-      eachTokenAsAction(tokenizer, function (action) {
+      while (nextToken = tokenizer.nextToken()) {
+        var action = tokenToAction(nextToken)
+
         if (currentDefinition) { // Are we currently defining a definition?
           addActionToCurrentDefinition(action);
         } else {
-          output += executeRuntimeAction(tokenizer, action);
+          output.push(executeRuntimeAction(tokenizer, action));
         }
-      });
+      }
     } catch (e) {
-      throwIfNotOneOf(e, [
-        MissingWordError,
-        StackUnderflowError,
-        UnbalancedControlStructureError
-      ]);
-
       currentDefinition = null;
       return "  " + e.message;
     }
 
     if (!currentDefinition) { // don't append output while definition is in progress
-      return " " + output + " ok";
+      return " " + output.join("") + " ok";
     }
   }
 
